bd515ae31ad629ec160981b355adfdcdf30b1f37
rvldrh <rivaldo0347t@gmail.com>
2024-12-29 21:51:05 +0700

    alhamdulillah

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 require("dotenv").config(); // Load .env file
 
 const app = express();
+const barang_masuk = require("./routes/barang_masuk.route");
+const barang_keluar = require("./routes/barang_keluar.route");
+const katalog_barang = require("./routes/katalog_barang.route");
+const laporan = require("./routes/laporan.route");
+const laporan_penjualan = require("./routes/laporan_penjualan.route");
 
 // Middleware
 app.use(express.json());
@@ -12,7 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("API is running on Vercel!");
 });
-
+app.use('/barang_masuk', barang_masuk);
+app.use('/barang_keluar', barang_keluar);
+app.use('/katalog_barang', katalog_barang);
+app.use('/laporan', laporan);
+app.use('/laporan_penjualan', laporan_penjualan);
 // Connect to MongoDB
 mongoose
   .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
