c92793d62bcf7cbff1e5f292fc9ff5112ac910a5
rvldrh <rivaldo0347t@gmail.com>
2024-12-29 21:10:56 +0700

    add vercel.json app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-
+ 
 const DB_URL = process.env.DB_URL; // Ambil dari environment variable
 const Schema = mongoose.Schema;
 const bodyParser = require("body-parser");
