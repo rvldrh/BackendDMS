0f423c4a6e69ca5d840c673a21399df1c9a1469e
rvldrh <rivaldo0347t@gmail.com>
2024-12-29 21:19:52 +0700

    fix: ready for deploy

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load .env file
+
 const app = express();
- 
-const DB_URL = process.env.DB_URL; // Ambil dari environment variable
-const Schema = mongoose.Schema;
-const bodyParser = require("body-parser");
-const cors = require("cors");
 
-const barang_masuk = require("./routes/barang_masuk.route");
-const barang_keluar = require("./routes/barang_keluar.route");
-const katalog_barang = require("./routes/katalog_barang.route");
-const laporan = require("./routes/laporan.route");
-const laporan_penjualan = require("./routes/laporan_penjualan.route");
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Middleware untuk parsing JSON
-app.use(cors());
-app.use(bodyParser.json());
+// Example routes
+app.get("/", (req, res) => {
+  res.send("API is running on Vercel!");
+});
 
-app.use('/barang_masuk', barang_masuk);
-app.use('/barang_keluar', barang_keluar);
-app.use('/katalog_barang', katalog_barang);
-app.use('/laporan', laporan);
-app.use('/laporan_penjualan', laporan_penjualan);
-
-// Koneksi ke MongoDB
+// Connect to MongoDB
 mongoose
-  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(3000, () =>
-      console.log("Server running on http://localhost:3000")
-    );
-  })
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+  .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Start the server (no need to specify port manually for Vercel)
+module.exports = app;
