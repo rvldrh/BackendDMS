2d4ad922437e3e0b244d27520efeed62a8cb2f2c
rvldrh <rivaldo0347t@gmail.com>
2025-01-01 16:36:29 +0700

    add: getKatalogBarangById

--- controllers/katalog_barang.conroller.js
+++ controllers/katalog_barang.conroller.js
@@ -15,6 +15,24 @@ exports.getKatalogBarang = async (req, res) => {
   }
 };
 
+exports.getKatalogBarangById = async (req, res) => {
+  try {
+      const katalog_barang = await ModelBarang.findById(req.params.id)
+          .populate('masuk', 'qty_masuk')
+          .populate('keluar', 'qty_keluar')
+          .exec();
+      if (!katalog_barang) {
+          return res.status(404).json({ message: "katalog_barang not found" });
+      }
+      res.status(200).json({
+          message: "katalog_barang retrieved successfully",
+          data: katalog_barang,
+      });
+  } catch (err) {
+      res.status(500).json({ message: "Error retrieving katalog_barang", error: err.message });
+  }
+};
+
 exports.addKatalogBarang = async (req, res) => {
   try {
       const { kode_barang, nama_barang, harga, satuan, stok_awal, masuk, keluar, stok_akhir } = req.body;
