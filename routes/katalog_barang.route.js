1d1d09c4eb65def2c4baefbaf014f1db2423b798
rvldrh <rivaldo0347t@gmail.com>
2025-01-02 07:34:39 +0700

    fix: typo for import

--- routes/katalog_barang.route.js
+++ routes/katalog_barang.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/katalog_barang.conroller");
+const controller = require("../controllers/katalog_barang.controller");
 
 router.get("/", controller.getKatalogBarang);
 router.post("/", controller.addKatalogBarang);
--- routes/laporan.route.js
+++ routes/laporan.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/lapora.controller");
+const controller = require("../controllers/laporan.controller");
 
 router.post("/", controller.addLaporan);
 router.get("/", controller.getLaporan);
