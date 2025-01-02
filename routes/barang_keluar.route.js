76dabe4e3d3459dd106c51349765c78db27badb4
rvldrh <rivaldo0347t@gmail.com>
2025-01-02 07:41:35 +0700

    fix: error

--- routes/barang_keluar.route.js
+++ routes/barang_keluar.route.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/barang_keluar.controller");
 
 router.get("/", controller.getBarangKeluar);
 router.post("/", controller.addBarangKeluar);
-router.get("/:id", controller.getBarangMasukById);
+router.get("/:id", controller.getBarangKeluarById);
 router.put("/:id", controller.updateBarangKeluar);
 
 module.exports = router;
--- routes/katalog_barang.route.js
+++ routes/katalog_barang.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/katalog_barang.controller");
+const controller = require("../controllers/katalog_barang.conroller");
 
 router.get("/", controller.getKatalogBarang);
 router.post("/", controller.addKatalogBarang);
