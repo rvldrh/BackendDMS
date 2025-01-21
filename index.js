8274b66e7d7d69c23f7a0d7243431521e18cf2a6
rvldrh <rivaldo0347t@gmail.com>
2025-01-21 20:39:36 +0700

    testing

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const laporan = require("./routes/laporan.route");
 const laporan_penjualan = require("./routes/laporan_penjualan.route");
 
 // Configure CORS with options
-app.use(cors({
-  origin: 'https://dms-bms-frontend.vercel.app/', 
-  methods: 'GET,POST,PUT,DELETE', 
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://dms-bms-frontend.vercel.app/");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});
 
 // Log DB_URL to check the environment variable
 console.log("DB_URL:", process.env.DB_URL);
