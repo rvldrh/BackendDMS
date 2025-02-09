c44d10f5add1a077079c2b8dff8adb2dd5f6e00d
rvldrh <rivaldo0347t@gmail.com>
2025-02-09 18:39:28 +0700

    ubah cors

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const laporan_penjualan = require("./routes/laporan_penjualan.route");
 
 // Configure CORS with options
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
