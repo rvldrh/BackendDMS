403810cf01a337a459e11e3739255ddfe4475522
rvldrh <rivaldo0347t@gmail.com>
2025-02-09 18:34:15 +0700

    tes

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ const laporan_penjualan = require("./routes/laporan_penjualan.route");
 
 // Configure CORS with options
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://dms-bms-frontend.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
   next();
