4ea69deb2dd8a1fbc261dfe67097c97199a76d3e
rvldrh <rivaldo0347t@gmail.com>
2025-02-12 13:59:25 +0700

    s

--- index.js
+++ index.js
@@ -36,6 +36,20 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Atau set ke 'https://dms-bms-frontend.vercel.app'
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
