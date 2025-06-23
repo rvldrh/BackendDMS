31f6ba436723f3f576b673f86e88aab51a077bb4
rvldrh <rivaldo0347t@gmail.com>
2025-06-23 16:44:47 +0700

    edit: update cors policy in index

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
     optionsSuccessStatus: 200,
